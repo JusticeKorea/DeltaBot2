18d53297af72ba8efa7fe41f0b85747d5a66a4b0
JusticeKorea <55412932+JusticeKorea@users.noreply.github.com>
2019-12-30 07:00:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,4 +10,4 @@ client.on("message", message => {
   if (message.content.startsWith("Ping")) message.channel.send("Pong");
 });
 
-client.login("NjYwODgyOTc2MTY2NTEwNjAy.Xgj4xA.9uCKuMMdA4QGm7gl-EvArn_rfyM");
+client.login(process.env.TOKEN);
